628dac678165d1a0f424daa8741d0178704770ea
Evgeny Arnautov <arnautov.eugene@gmail.com>
2019-10-16 17:50:05 +0500

    Nice naming for CSVHandle made by headers

--- src/main/java/net/ddns/arnautovevgeny/pricelist/ProductFromCSVRowByHeaders.java
+++ src/main/java/net/ddns/arnautovevgeny/pricelist/ProductFromCSVRowByHeaders.java
@@ -2,12 +2,12 @@ package net.ddns.arnautovevgeny.pricelist;
 
 import de.siegmar.fastcsv.reader.CsvRow;
 
-public class ProductFromCSVRowHeaders extends Product {
-    interface ProductCSVRowHeadersCreator extends Product.ProductCSVCreator {
-        ProductFromCSVRowHeaders create(CsvRow row);
+public class ProductFromCSVRowByHeaders extends Product {
+    interface ProductCSVRowByHeadersCreator extends Product.ProductCSVCreator {
+        ProductFromCSVRowByHeaders create(CsvRow row);
     }
 
-    public ProductFromCSVRowHeaders(CsvRow row) {
+    public ProductFromCSVRowByHeaders(CsvRow row) {
         super(Integer.parseInt(row.getField("product ID")),
                 row.getField("Name"),
                 row.getField("Condition"),
